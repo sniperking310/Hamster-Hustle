7576e2dcbd70581668c2c6c4611bbc0d30ce37ea
Betim <94181611+sniperking310@users.noreply.github.com>
2024-03-27 07:41:44 +0100

    LevelLoader verbessert
    -Quit Game implementiert
    -LevelLoader bleibt in jeder Scene
    -kommentiert

--- Hamster Hustle/Assets/SceneLoader.cs
+++ Hamster Hustle/Assets/SceneLoader.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void Start()
+    {
+        //Verhindert, dass der LevelLoader bei einer neuen Scene zerstört wird
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     public void LoadScene(int index)
     {
+        //Wechselt die Scene nach dem Index der Scene
         SceneManager.LoadSceneAsync(index);
+    }
+
+    public void EndGame()
+    {
+        //Beendet das Spiel (nur nach build)
+        Application.Quit();
     }
 }
