b09dd479a76c0873b0f61db6aae6ab0d9feb5b48
Betim <94181611+sniperking310@users.noreply.github.com>
2024-04-10 20:15:21 +0200

    Fade Animation teilweise implementiert

--- Hamster Hustle/Assets/Scripts/SceneLoader.cs
+++ Hamster Hustle/Assets/Scripts/SceneLoader.cs
@@ -5,16 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void Start()
+
+    public static SceneLoader Instance;
+    public Animator transition;
+    public float transitionTime = 0.5f;
+
+    public void Awake()
     {
-        //Verhindert, dass der LevelLoader bei einer neuen Scene zerstört wird
-        DontDestroyOnLoad(this.gameObject);
+        //Sorgt dafür, dass es nur einen SceneLoader gibt
+        if(Instance == null)
+        {
+            Instance = this;
+            //Verhindert, dass der LevelLoader bei einer neuen Scene zerstört wird
+            DontDestroyOnLoad(this.gameObject);
+        }else
+        {
+            //Falls es mehrere SceneLoader gibt, werden sie zerstört
+            Destroy(gameObject);
+        }
     }
 
-    public void LoadScene(int index)
+    //Läd eine Scene mit dem index einer Scene
+    IEnumerator LoadScene(int index)
     {
+        //Spielt Animation für den Fade
+        transition.SetTrigger("FadeOut");
+
+        //Warte die Dauer der Animation ab
+        yield return new WaitForSeconds(transitionTime);
+
         //Wechselt die Scene nach dem Index der Scene
         SceneManager.LoadSceneAsync(index);
+    }
+
+    //Notwendig, weil Coroutines so gestartet werden müssen
+    public void StartLoadScene(int index) 
+    {
+        StartCoroutine(LoadScene(index));
     }
 
     public void EndGame()
