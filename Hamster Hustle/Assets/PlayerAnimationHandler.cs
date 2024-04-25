04b94c094f520f6701e5ff0bfcd928a52beb7747
Betim <94181611+sniperking310@users.noreply.github.com>
2024-04-25 11:32:36 +0200

    Fixed Player Animation

--- Hamster Hustle/Assets/PlayerAnimationHandler.cs
+++ Hamster Hustle/Assets/PlayerAnimationHandler.cs
@@ -7,29 +7,48 @@ public class PlayerAnimationHandler : MonoBehaviour
     public Rigidbody2D rb;
     public Jumping jumping;
     public Animator animator;
+    private Vector3 newScale;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         jumping = GetComponent<Jumping>();
+        newScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GiveParams();
+        HandleAnimation();
+        PlayerFacing();
     }
 
-    public void GiveParams()
+    public void HandleAnimation()
     {
         float ySpeed = rb.velocity.y;
 
         animator.SetFloat("ySpeed", ySpeed);
 
-        float xSpeed = rb.velocity.x;
+        float xSpeed = Mathf.Abs(Input.GetAxisRaw("Horizontal"));
 
         animator.SetFloat("xSpeed", xSpeed);
 
         animator.SetBool("GroundCheck", jumping.Groundcheck());
+    }
+
+    public void PlayerFacing()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+
+        if (xMovement > 0)
+        {
+            newScale.x = xMovement;
+        }
+        else if (xMovement < 0)
+        {
+            newScale.x = xMovement;
+        }
+
+        transform.localScale = newScale;
     }
 }
