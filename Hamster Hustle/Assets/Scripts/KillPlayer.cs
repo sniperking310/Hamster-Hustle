473943ea5148e5596fecf158d7d2a44b8ef72f4e
FranziskaSuess <franziska.suess@gmx.de>
2024-04-28 13:00:56 +0200

    added game over functionality

--- Hamster Hustle/Assets/Scripts/KillPlayer.cs
+++ Hamster Hustle/Assets/Scripts/KillPlayer.cs
@@ -1,16 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class KillPlayer : MonoBehaviour
 {
     public GameObject player;
+    public GameObject gameOverUI;
     public Transform respawnPoint;
     
-    // setzt bei Kollision die Position des Players auf den Respawn Punkt zurück
+    // öffnet bei Kollision das Game-Over Interface
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")){
             player.transform.position = respawnPoint.position;
+            gameOverUI.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
+
+    // startet das Level neu
+    public void Restart()
+    {
+        gameOverUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    // leitet zum Menü weiter
+    public void LoadMenu(int index)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(index);
+    }
+
 }
